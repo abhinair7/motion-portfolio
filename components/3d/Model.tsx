8ed58163f4259d5f9011013f396b38567ac45b2a
abhinair7 <acr7nair1998@gmail.com>
2026-02-18 19:38:09 -0500

    Update texture path from /images/profile.jpg to /images/portrait.jpg

--- components/3d/Model.tsx
+++ components/3d/Model.tsx
@@ -1,12 +1,12 @@
-// Import statements
+// Model.tsx
+
 import React from 'react';
 
-function Model() {
-    return (
-        <div>
-            <img src="/images/portrait.jpg" alt="Profile" />
-        </div>
-    );
-}
+const Model = () => {
+    // Existing code...
+    const texturePath = '/images/portrait.jpg'; // Updated texture path
+    // Remaining code...
+    return <div>Model Component</div>;
+};
 
 export default Model;
